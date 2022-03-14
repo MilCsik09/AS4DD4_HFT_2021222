2e416130b4d54c08d4d8c5ed8c688cdb4999b622
csikm <csikm@MILAN_LAPTOP>
2022-03-14 21:38:22 +0100

    Add project files.

---
+++ AS4DD4_HFT_2021222.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AS4DD4_HFT_2021222.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
