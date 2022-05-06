afd413a9343b55f5141d4721aaa3730055a397d0
MilCsik09 <csik.milan@stud.uni-obuda.hu>
2022-05-06 23:27:32 +0200

    Logics created.

---
+++ AS4DD4_HFT_2021222.Logic/BrandLogic.cs
@@ -0,0 +1,40 @@
+﻿using AS4DD4_HFT_2021222.Models;
+using AS4DD4_HFT_2021222.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS4DD4_HFT_2021222.Logic
+{
+    public class BrandLogic : IComputerRepairLogic<Brand<Type>>
+    {
+        IComputerRepairRepository<Brand<Type>> repo;
+
+        public void Create(Brand<Type> t)
+        {
+            repo.Create(t);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public IQueryable<Brand<Type>> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public Brand<Type> ReadOne(int id)
+        {
+            return repo.ReadOne(id);
+        }
+
+        public void Update(Brand<Type> t)
+        {
+            repo.Update(t);
+        }
+    }
+}
---
+++ AS4DD4_HFT_2021222.Logic/ComputerLogic.cs
@@ -0,0 +1,40 @@
+﻿using AS4DD4_HFT_2021222.Models;
+using AS4DD4_HFT_2021222.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS4DD4_HFT_2021222.Logic
+{
+    public class ComputerLogic : IComputerRepairLogic<Computer>
+    {
+        IComputerRepairRepository<Computer> repo;
+
+        public void Create(Computer t)
+        {
+            repo.Create(t);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public IQueryable<Computer> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public Computer ReadOne(int id)
+        {
+            return repo.ReadOne(id);
+        }
+
+        public void Update(Computer t)
+        {
+             repo.Update(t);
+        }
+    }
+}
