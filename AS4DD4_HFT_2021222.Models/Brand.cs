5bf8ca457cc7fb54986c52b169956a6f504e2b4c
MilCsik09 <csik.milan@stud.uni-obuda.hu>
2022-05-23 19:03:25 +0200

    Minor fixes

--- AS4DD4_HFT_2021222.Models/Brand.cs
+++ AS4DD4_HFT_2021222.Models/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace AS4DD4_HFT_2021222.Models
 {
     public class Brand : Entity
     {
+        [Required]
         public string Name { get; set; }  
         public virtual ICollection<CPU> CpuProducts { get; set; }
         public virtual ICollection<VGA> VgaProducts { get; set; }
--- AS4DD4_HFT_2021222.Models/CPU.cs
+++ AS4DD4_HFT_2021222.Models/CPU.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,14 @@ namespace AS4DD4_HFT_2021222.Models
 {
     public class CPU : Entity
     {
+        [Required]
         public string Model { get; set; }  
         public int Price { get; set; }
         [NotMapped]
         [JsonIgnore]
         public virtual Brand Brand { get; set; }
+        public bool isUsed { get; set; }
+        public bool isOperational { get; set; }
 
         [ForeignKey(nameof(Brand))]
         public int BrandId { get; set; }
